4c1bb8976f1e16d8f3f80f4fc7436214cb2c48da
DummyDa <bacabacum51633@gmail.com>
2025-06-07 10:43:12 +0300

    Добавил подключение к ДБ

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from dotenv import load_dotenv
 import os
 import psycopg2
 
+load_dotenv
+
+conn = psycopg2.connect(
+        dbname=os.getenv("DBNAME"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        host=os.getenv("HOST"),
+        port=os.getenv("PORT"),
+        )
+
 app = Flask(__name__)
 
 def main():
