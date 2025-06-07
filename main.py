dc37d489735371599a7968255a09324630873b1f
DummyDa <bacabacum51633@gmail.com>
2025-06-07 11:04:51 +0300

    Добавил роут form

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ conn = psycopg2.connect(
 
 app = Flask(__name__)
 
+@app.route("/form", methods=["GET", "POST"])
+def forms():
+    if request.method == "POST":
+        name = request.form["name"]
+        achievements = request.form["achievements"]
+        try:
+            with conn.cursor() as cur:
+                cur.execute("INSERT INTO scientists (name, achievements) VALUES (%s, %s)", (name, achievements),)
+                conn.commit()
+                return "Успешно"
+        except Exception as e:
+            conn.rollback()
+            print("проблема с ", e)
+            return "Ошибка"
+    return render_template("form.html")
+
 def main():
     app.run(debug=True)
 
